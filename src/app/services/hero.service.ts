959a6497ea8b9beecf94848d98db8a9e54803ebe
shforeal <rivena14798@gmail.com>
2022-04-29 15:41:26 +0300

    exp: comment on the inject

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HEROES } from '../mock-heroes';
 import { Hero } from '../models/hero';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  //Modify the constructor with a parameter that declares a private messageService property.
+  //Angular will inject the singleton MessageService into that property when it creates
+  // the HeroService.
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES); //of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
