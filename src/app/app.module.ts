a2c81b42211127f5a4e40e08dc7a39a89bc143fd
shforeal <rivena14798@gmail.com>
2022-04-29 12:39:45 +0300

    feat: separate the detail logic to a  new cmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './cmps/heroes/heroes.component';
+import { HeroDetailComponent } from './cmps/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
